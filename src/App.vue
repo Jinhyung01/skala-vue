6e727785607d2905f12c14fc11014f22b48aa4cd
이진형 <dlwls7405@naver.com>
2026-08-18 17:42:25 +0900

    Vue 디렉티브와 동적 속성 바인딩 예제 구현

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script setup>
-import SampleOne from './components/practices/basic/SampleOne.vue'
-import SampleTwo from './components/practices/basic/SampleTwo.vue'
+import BindBasic from './components/practices/directive/BindBasic.vue'
+import BindClass from './components/practices/directive/BindClass.vue'
+import BindShorthand from './components/practices/directive/BindShorthand.vue'
+import BindStyle from './components/practices/directive/BindStyle.vue'
+import DirectiveHtml from './components/practices/directive/DirectiveHtml.vue'
+import DirectiveHtmlXss from './components/practices/directive/DirectiveHtmlXss.vue'
+import DirectiveText from './components/practices/directive/DirectiveText.vue'
 </script>
 
 <template>
   <div style="padding: 20px">
-    <SampleOne />
-    <SampleTwo />
+    <DirectiveHtml />
+    <DirectiveHtmlXss />
+    <DirectiveText />
+    <BindBasic />
+    <BindClass />
+    <BindStyle />
+    <BindShorthand />
   </div>
 </template>
---
+++ src/components/practices/directive/DirectiveHtmlXss.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+const inputValue = ref('')
+const message = ref('')
+
+const showMessage = () => {
+  message.value = inputValue.value
+}
+</script>
+
+<template>
+  <div class="practice-section">
+    <h2>v-html XSS 학습</h2>
+    <input v-model="inputValue" placeholder="내용을 입력하세요" />
+    <button @click="showMessage">확인</button>
+    <div v-html="message"></div>
+    <p>※ 사용자가 입력한 값을 v-html로 출력하면 XSS 공격에 노출될 수 있습니다.</p>
+  </div>
+</template>
